a33cc293712ddaf1b2e0ecd4744979d578ad1a29
anon anon <develop123456789@gmail.com>
2022-09-24 18:38:07 +0900

    delete unused files.

--- src/gen/d_ts/settings.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Settings {
-    settings: object[];
-    constructor(settings: object[]);
-    createSettings(): void;
-    restorSettings(): void;
-    setSettings(settings: object): void;
-    deleteSettings(ele: Element): void;
-    saveSettings(): void;
-}
